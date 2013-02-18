bc43e67743d438ed95f098e31317d40832e5f902
Karl Hammar <karl@karlhammar.com>
2013-02-18 15:59:50 +0100

    Restructured maven project for plugins.

--- plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/ELProfile.java
+++ plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/ELProfile.java
@@ -1,10 +1,20 @@
 package com.karlhammar.ontometrics.plugins;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
 
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.profiles.OWL2ELProfile;
+import org.semanticweb.owlapi.profiles.OWLProfileReport;
+
 public class ELProfile implements OntoMetricsPlugin {
+	
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	public String getName() {
 		return "EL Profile Plugin";
@@ -19,8 +29,17 @@ public class ELProfile implements OntoMetricsPlugin {
 	}
 
 	public String getMetricValue(File ontologyFile) {
-		// TODO Auto-generated method stub
-		return "TRUE";
+		try {
+			OWL2ELProfile o2el = new OWL2ELProfile();
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			OWLOntology ontology = manager.loadOntologyFromOntologyDocument(ontologyFile);
+			OWLProfileReport report = o2el.checkOntology(ontology);
+			return new Boolean(report.isInProfile()).toString();
+		} 
+		catch (OWLOntologyCreationException e) {
+			logger.severe(e.getMessage());
+			return null;
+		}
 	}
 
 }
