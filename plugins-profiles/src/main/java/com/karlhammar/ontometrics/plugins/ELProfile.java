c71dae253f929acfe928186fb26b84f8c378ac58
Karl Hammar <karl@karlhammar.com>
2013-02-18 14:11:12 +0100

    Removed init message from profile plugin classes.

--- plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/ELProfile.java
+++ plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/ELProfile.java
@@ -1,20 +1,17 @@
 package com.karlhammar.ontometrics.plugins;
 
 import java.io.File;
-import java.util.logging.Logger;
 
 import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
 
 public class ELProfile implements OntoMetricsPlugin {
-
-	private static Logger logger = Logger.getLogger(ELProfile.class.getName());
 	
 	public String getName() {
 		return "EL Profile Plugin";
 	}
 
 	public void init() {
-		logger.info(getName() + " initialized!");
+		
 	}
 
 	public String getMetricAbbreviation() {
@@ -23,7 +20,7 @@ public class ELProfile implements OntoMetricsPlugin {
 
 	public String getMetricValue(File ontologyFile) {
 		// TODO Auto-generated method stub
-		return null;
+		return "TRUE";
 	}
 
 }
--- plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/QLProfile.java
+++ plugins-profiles/src/main/java/com/karlhammar/ontometrics/plugins/QLProfile.java
@@ -1,20 +1,17 @@
 package com.karlhammar.ontometrics.plugins;
 
 import java.io.File;
-import java.util.logging.Logger;
 
 import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
 
 public class QLProfile implements OntoMetricsPlugin {
-
-	private static Logger logger = Logger.getLogger(QLProfile.class.getName());
 	
 	public String getName() {
 		return "QL Profile Plugin";
 	}
 
 	public void init() {
-		logger.info(getName() + " initialized!");
+		
 	}
 
 	public String getMetricAbbreviation() {
@@ -23,7 +20,7 @@ public class QLProfile implements OntoMetricsPlugin {
 
 	public String getMetricValue(File ontologyFile) {
 		// TODO Auto-generated method stub
-		return null;
+		return "FALSE";
 	}
 
 }
