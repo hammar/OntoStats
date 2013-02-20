4463dd4ffefa6de0e69283ae2774b37f7e10b3f2
Karl Hammar <karl@karlhammar.com>
2013-02-20 10:55:26 +0100

    Added TIP metric to structural indicators plugin Metric originally from Zhang et al 2010.

---
+++ plugins-structural/src/main/java/com/karlhammar/ontometrics/plugins/structural/TreeImpurity.java
@@ -0,0 +1,66 @@
+package com.karlhammar.ontometrics.plugins.structural;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
+
+public class TreeImpurity implements OntoMetricsPlugin {
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+	private StructuralSingletonJena ss;
+	
+	public String getName() {
+		return "Tree impurity plugin";
+	}
+
+	public void init(File ontologyFile) {
+		ss = StructuralSingletonJena.getSingletonObject(ontologyFile);
+
+	}
+
+	public String getMetricAbbreviation() {
+		return "TreeImp";
+	}
+
+	public String getMetricValue(File ontologyFile) {
+		System.out.println("RUNNING GETMETRICVALUE");
+		if (null == ss) {
+			logger.info("getMetricValue called before init()!");
+			init(ontologyFile);
+		}
+		OntModel ontology = ss.getOntology();
+		Integer subClassEdges = 0;
+		Integer classNodes = 0;
+		
+		Property subClassOfProperty = ResourceFactory.createProperty("http://www.w3.org/2000/01/rdf-schema#subClassOf");
+		Selector selector = new SimpleSelector(null, subClassOfProperty, (RDFNode)null);
+		StmtIterator subClassIter = ontology.listStatements(selector);
+		while (subClassIter.hasNext()) {
+			Statement s = subClassIter.next();
+			System.out.println("Subclass statement: " + s.toString());
+			subClassEdges++;
+		}
+		
+		ExtendedIterator<OntClass> allClasses = ontology.listClasses();
+		while (allClasses.hasNext()) {
+			OntClass c = allClasses.next();
+			System.out.println("Class " + c.toString());
+			classNodes++;
+		}
+		
+		Integer TIP = subClassEdges - classNodes + 1;
+		return TIP.toString();
+	}
+
+}
