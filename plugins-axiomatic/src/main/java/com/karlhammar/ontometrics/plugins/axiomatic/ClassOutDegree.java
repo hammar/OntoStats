0ab3f168a1eec542e72c66c23be56692c19e6854
Karl Hammar <karl@karlhammar.com>
2013-02-20 09:32:54 +0100

    Added Class Out-Degree metric to axiomatic plugin

---
+++ plugins-axiomatic/src/main/java/com/karlhammar/ontometrics/plugins/axiomatic/ClassOutDegree.java
@@ -0,0 +1,58 @@
+package com.karlhammar.ontometrics.plugins.axiomatic;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.iterator.ExtendedIterator;
+import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
+
+public class ClassOutDegree implements OntoMetricsPlugin {
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+	private StructuralSingleton ss;
+	
+	public String getName() {
+		return "Class out-degree plugin";
+	}
+
+	public void init(File ontologyFile) {
+		ss = StructuralSingleton.getSingletonObject(ontologyFile);
+	}
+
+	public String getMetricAbbreviation() {
+		return "COutDegree";
+	}
+
+	public String getMetricValue(File ontologyFile) {
+		if (null == ss) {
+			logger.info("getMetricValue called before init()!");
+			init(ontologyFile);
+		}
+		OntModel ontology = ss.getOntology();
+		ExtendedIterator<OntClass> allClasses = ontology.listClasses();
+		int nrClasses = 0;
+		int outEdges = 0;
+		while (allClasses.hasNext()) {
+			OntClass c = allClasses.next();
+			nrClasses++;
+			Selector selector = new SimpleSelector(c, null, (RDFNode)null);
+			StmtIterator iter = ontology.listStatements(selector);
+			while (iter.hasNext()) {
+				Statement s = iter.next();
+				if (s.getObject().isResource()) {
+					outEdges++;
+				}
+			}
+		}
+		Float outDegree = (float)outEdges / nrClasses;
+		return outDegree.toString();
+	}
+
+}
