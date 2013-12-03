274f23c88f457289bb6d47d827e5f7eb3f6b3da0
Aidan Delaney <aidan@phoric.eu>
2013-12-03 18:14:49 +0000

    Added diagrammetrics related to disjunction.

---
+++ plugins-diagrammetrics/src/main/java/org/ontologyengineering/ontometrics/plugins/DisjSubsetOnly.java
@@ -0,0 +1,32 @@
+package org.ontologyengineering.ontometrics.plugins;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import com.karlhammar.ontometrics.plugins.api.OntoMetricsPlugin;
+
+public class DisjSubsetOnly implements OntoMetricsPlugin {
+    private Logger logger = Logger.getLogger(getClass().getName());
+    private SimpleQuery sq;
+
+    public String getName() {
+        return "Ratio of (Atom Disj Atom) Subsumes Only axioms to the TBox size";
+    }
+
+    public void init(File ontologyFile) {
+        try {
+            sq = new SimpleQuery(ontologyFile, "simple_disjsubsetonly.sparql");
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    public String getMetricAbbreviation() {
+        return "DisjSubsetOnly";
+    }
+
+    public String getMetricValue(File ontologyFile) {
+        return sq.calculatePrettyDiagramRatio();
+    }
+}
