d0dcec86d5d7f4ecac022b2809183a4b01ed17ce
Aidan Delaney <aidan@phoric.eu>
2013-12-11 15:13:21 +0000

    Quick hack of a shell script that runs OntoMetrics over a list of files.

---
+++ batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ONTOSTATS='java -jar ontometrics-core/target/uber-ontometrics-core-0.0.1-SNAPSHOT.jar'
+
+# remove plugins
+rm plugins/uber-*
+
+# copy all plugins to plugins
+find . -name "uber-plugins-*-SNAPSHOT.jar" -exec cp '{}' plugins \; 2>/dev/null
+
+# run the firs file and generated table headers
+$ONTOSTATS -n -f $1 2>/dev/null
+
+# now run for every other file
+for item in "${@:2}"
+do
+    $ONTOSTATS -n -h -f $item 2>/dev/null
+done
