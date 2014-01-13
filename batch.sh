d7ab017c6cbc6703769ec2bf7e5ed9926b77b52c
Aidan Delaney <aidan@phoric.eu>
2014-01-13 13:55:27 +0000

    Slightly better error reporting in batch runner.

--- batch.sh
+++ batch.sh
@@ -10,9 +10,15 @@ find . -name "uber-plugins-*-SNAPSHOT.jar" -exec cp '{}' plugins \; 2>/dev/null
 
 # run the firs file and generated table headers
 $ONTOSTATS -n -f $1 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo "$1 failed to load"
+fi
 
 # now run for every other file
 for item in "${@:2}"
 do
     $ONTOSTATS -n -h -f $item 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo "$item failed to load"
+    fi
 done
