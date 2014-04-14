e1d5f6074a055938e251398455d7f87e99a10d28
Aidan Delaney <aidan@ontologyengineering.org>
2014-04-14 10:09:28 +0100

    Replaced areAtomic with a more general filterPair

--- plugins-diagrammetrics/src/main/java/org/ontologyengineering/ontometrics/plugins/OWLAPIQuery.java
+++ plugins-diagrammetrics/src/main/java/org/ontologyengineering/ontometrics/plugins/OWLAPIQuery.java
@@ -33,11 +33,19 @@ public class OWLAPIQuery {
         // Find all subset axioms
         Set<OWLSubClassOfAxiom> as = owlapi.getOntology().getAxioms(AxiomType.SUBCLASS_OF);
 
-        // Filter LHS
-        as = as.stream().filter(a -> isAtomic(a.getSubClass())).collect(Collectors.toSet());
+        switch(lhs) {
+            // Filter LHS
+        case ATOM:
+            as = as.stream().filter(a -> isAtomic(a.getSubClass())).collect(Collectors.toSet());
+            break;
+        }
 
-        // Filter RHS
-        as = as.stream().filter(a -> isAtomic(a.getSuperClass())).collect(Collectors.toSet());
+        switch(rhs) {
+            // Filter RHS
+        case ATOM:
+            as = as.stream().filter(a -> isAtomic(a.getSuperClass())).collect(Collectors.toSet());
+            break;
+        }
 
         // Find all equality axioms
         Set<OWLEquivalentClassesAxiom> es = owlapi.getOntology().getAxioms(AxiomType.EQUIVALENT_CLASSES);
@@ -45,15 +53,45 @@ public class OWLAPIQuery {
                 .flatMap(a -> a.asPairwiseAxioms().stream())
                 .collect(Collectors.toSet());
 
+        
         // Filter based on whether each pair is a pairing of atomic classes
-        pairwise = pairwise.stream().filter(a -> areAtomic(a.getClassExpressions())).collect(Collectors.toSet());
+        pairwise = pairwise.stream().filter(a -> filterPair(a.getClassExpressions())).collect(Collectors.toSet());
 
         // return size of filtered subsets + (2 * size of filtered equalities)
         return new Double(as.size() + (2.0 * pairwise.size())).toString();
     }
 
-    private boolean areAtomic(Set<OWLClassExpression> cs) {
-        return cs.stream().allMatch(c -> isAtomic(c));
+    private boolean filterPair(Set<OWLClassExpression> ocs) {
+        // One of these must match lhs, the other rhs
+        // TODO: make this code less fugly
+
+        // we can't assume anything about the order of the class expressions, 
+        // but the procedural order of the code checks an element for lhs match
+        // first.
+        OWLClassExpression lhsExpr = null;
+        boolean lhsMatched = false, rhsMatched = false;
+        for(OWLClassExpression ce: ocs) {
+            if(!lhsMatched) {
+                switch(lhs) {
+                case ATOM:
+                    if(isAtomic(ce)) {
+                        lhsExpr=ce;
+                        lhsMatched = true;
+                    }
+                    break;
+                }
+            }
+
+            switch(rhs) {
+            case ATOM:
+                if(isAtomic(ce) && ce != lhsExpr) {
+                    rhsMatched = true;
+                }
+                break;
+            }
+        }
+
+        return lhsMatched && rhsMatched;
     }
 
     // for us, "atomic" asks whether a class is named or not.
